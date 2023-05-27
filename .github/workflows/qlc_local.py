3a160f396a9853a7b117e31ec201f9c6069f76d6
Ric Glassey <rjglasse@gmail.com>
2023-05-27 14:08:41 +0000

    Update spacing (it works!)

--- .github/workflows/qlc_local.py
+++ .github/workflows/qlc_local.py
@@ -30,8 +30,8 @@ def main(key, files):
 
         # Open issue on the repository with the question
         title = "🤖 Answer this question about your code!"
-        body = ("**Considering [`/" + source_file + "`](../blob/master/" + source_file + ")**" + " <br /> " +
-            "**" + response_json['question'] + "**" + " <br /> " +
+        body = ("**Considering [`/" + source_file + "`](../blob/master/" + source_file + ")**" + " <br /> <br /> " +
+            "**" + response_json['question'] + "**" + " <br /> <br /> " +
             "A: " + response_json['answer1'] + " <br /> " +
             "<details><summary>...</summary>" + "_Explanation: " + response_json['explanation1'] + "_" + " </details>" +
             "B: " + response_json['answer2'] + " <br /> " +
