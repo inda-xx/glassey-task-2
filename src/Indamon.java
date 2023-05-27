b2709a63cf99ab872a7a01d5e731582762deaa23
Ric Glassey <rjglasse@gmail.com>
2023-05-27 06:55:16 +0000

    Add Indamon.java

---
+++ src/Indamon.java
@@ -0,0 +1,25 @@
+class Indamon {
+
+    String name;
+    int hp;
+    int attack;
+    int defense;
+    boolean fainted;
+
+    public static void main(String[] args) {
+        // create a new "Student" object
+        Indamon glassey = new Indamon();
+    
+        // assign the instance variables to meaningful values
+        glassey.name = "Glassey";
+        glassey.hp = 10;
+        glassey.attack = 5;
+        glassey.defense = 5;
+    
+        // get the information of the assigned values
+        System.out.println("Name: " + glassey.name);
+        System.out.println("HP: " + glassey.hp);
+        System.out.println("Attack value: " + glassey.attack);
+        System.out.println("Defense value: " + glassey.defense);
+      } // end main method
+}
