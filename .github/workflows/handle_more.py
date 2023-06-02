f5890b9f2f76f7df29438817bc529a123cf809a8
Ric Glassey <rjglasse@gmail.com>
2023-06-03 01:46:22 +0200

    Update handle_more.py

--- .github/workflows/handle_more.py
+++ .github/workflows/handle_more.py
@@ -16,16 +16,16 @@ def main(key, file_path):
         messages=[
             {"role": "system", "content": "You are a teacher that wants to help a student by extending their programming task with a fun bonus exercise. Here is their overall task:"},
             {"role": "assistant", "content": readme},
-            {"role": "assistant", "content": "Provide the next exercise in markdown format."},
+            {"role": "assistant", "content": "Provide the next exercise."},
         ]
     )
     
     exercise = response.choices[0]['message']['content']
-    print(codecs.escape_decode(bytes(exercise, "utf-8"))[0].decode("utf-8"))
+    print(exercise)
     
     # Set the issue title and body
     title = "🤖 Here is a bonus exercise for you!"
-    body = codecs.escape_decode(bytes(exercise, "utf-8"))[0].decode("utf-8")
+    body = exercise
 
     print(f"::set-output name=title::{title}")
     print(f"::set-output name=body::{body}")
