e645edb3f5f5af9eae4ebcf84cd1742bf9e5f422
Ric Glassey <rjglasse@gmail.com>
2023-06-03 00:12:04 +0200

    Update handle_more.py

--- .github/workflows/handle_more.py
+++ .github/workflows/handle_more.py
@@ -24,6 +24,8 @@ def main(key, file_path):
     # Extract the exercise from the response
     exercise = response.choices[0]['message']['content']
     response_json = json.loads(exercise)
+    
+    print(json.stringify(response_json))
 
     # Set the issue title and body
     title = "🤖 Here is a bonus exercise for you!"
