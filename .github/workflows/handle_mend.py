c8e113016d9dc747465e846cf7bf5cff1eeb67e4
Ric Glassey <rjglasse@gmail.com>
2023-06-05 10:17:13 +0200

    Update handle_mend.py

--- .github/workflows/handle_mend.py
+++ .github/workflows/handle_mend.py
@@ -1,8 +1,30 @@
-def main():
-    title = "🤖 Here's how you can improve your code!"
-    body = "This is a dummy body"
-    print(f"::set-output name=title::{title}")
-    print(f"::set-output name=body::{body}")
+import os
+import sys
+import openai
+import json
+
+def main(key, file_path):
+    openai.api_key = key
+
+    with open(file_path, 'r') as file:
+        source_code = file.read()
+        
+    # Call openai api to generate question
+    # See: https://platform.openai.com/docs/guides/chat/introduction for more information on the call
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are a teacher that wants to help a student by improving their code quality and code comprehensibility."},
+            {"role": "assistant", "content": "Here is the code they want help with:"}
+            {"role": "assistant", "content": source_code},
+            {"role": "assistant", "content": "If the above code is not Java source code, then it is ok to tell them that the file they submitted was not a recognised Java file and they should try again."},
+            {"role": "assistant", "content": "If the above code is Java source code, then give them a short list of code quality improvements that help improve code comprehensibility."},
+            {"role": "assistant", "content": "Format the response in markdown format"},
+        ]
+    )
+    fix_apostrophe = response.choices[0]['message']['content'].replace("\'","'")
+    remove_quotes = json.dumps(fix_apostrophe)[1:-1]
+    print(remove_quotes)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1], sys.argv[2])
