8e302d6cd81868b5615f2162960f21a3129e4ff9
Ric Glassey <rjglasse@gmail.com>
2024-06-11 14:52:31 +0200

    Update handle_ama.py

--- .github/workflows/handle_ama.py
+++ .github/workflows/handle_ama.py
@@ -1,13 +1,15 @@
 import os
 import sys
-import openai
 import json
 import random
 
+from openai import OpenAI
+
 def main(key):
-    openai.api_key = key
+
+    client = OpenAI(api_key=key)
     
-    # Strategy 2: Pick a random file from all java files found in repo
+    # Pick a random file from all java files found in repo
     source_file_list = [line.strip() for line in sys.stdin]
     source_file = random.choice(source_file_list)
     with open(source_file, 'r') as file:
@@ -15,15 +17,16 @@ def main(key):
         
     # Call openai api to generate question
     # See: https://platform.openai.com/docs/guides/chat/introduction for more information on the call
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
+    response = client.chat.completions.create(
         messages=[
             {"role": "system", "content": "You are a teacher that wants to help a student understand their programming assignment."},
             {"role": "assistant", "content": "Given the following student source code, generate a multiple choice question about the code to test understanding of the code. The question should have three answer options and explanations for each answer option.\n\n"},
             {"role": "assistant", "content": source_code},
             {"role": "assistant", "content": "The response should be formatted as a json object with the following fields: question, answer1, answer2, answer3, explanation1, explanation2, explanation3."},
-        ]
+        ],
+        model="gpt-4",
     )
+    
     response_json = json.loads(response.choices[0]['message']['content'])
 
     # Print issue body
